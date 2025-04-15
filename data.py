cd16a61d4e619284c2c0ab3395cb57395527dc71
Your Name <you@example.com>
2025-04-15 10:27:48 +0500

    first commit

---
+++ data.py
@@ -0,0 +1,27 @@
+school_db = [
+    {
+        "title": "PDP School",
+        "room": [1, 2, 3, 4, 5, 6],
+        "teacher": ["Musharraf", "G'ayrat", "Shaxlo", "Muzaffar"] # noqa
+    },
+    {
+        "title": "IT School",
+        "room": [1, 2, 3, 4, 5, 6],
+        "teacher": ["Bekzod", "Ali", "Javohir"] # noqa
+    }
+]
+
+student_db = [
+    {
+        "name": "Samandar",
+        "email": "samandar@gmail.com",
+        "room_id": 1,
+        "since": ["Python", "Math", "English"] # noqa
+    },
+    {
+        "name": "Javohir",
+        "email": "javohir@gmail.com",
+        "room_id": 2,
+        "since": ["Python", "Math", "English", "Chemistry"] # noqa
+    }
+]
